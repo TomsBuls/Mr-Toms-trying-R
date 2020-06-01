65356e61491826e1e11d90d749f019c88cf71700
Toms Buls <tomsbuls11@gmail.com>
2020-06-01 11:29:48 +0300

    commiting new changes to answer correctly on quiz

--- Toms testing.R
+++ Toms testing.R
@@ -3,3 +3,5 @@ print ("this file was created within RStudio")
 print ("and now it lives on GitHub")
 
 print ("I added new changes to check my understanding")
+
+print ("testing again to complete quiz")
